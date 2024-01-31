8cf334931e0f20336a80f8268181a4d476b05c5e
danielhill1982 <mail@dhill.dev>
2024-01-31 03:57:34 +0100

    bla

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="p-9 bg-red-600 container mx-auto">
+    <h1 class="text-6xl">Energieprofi-Tenner</h1>
+    </div>
+  </div>
+</template>
+
+<style>
+body {
+  font-family: Prompt;
+}
+</style>
